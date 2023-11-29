993a52d84693c36fa9b93ecc53e61ddbf48863b2
Benerman <benjaminwlarson@gmail.com>
2023-11-28 19:33:42 -0500

    further animation tweaks

--- js/index.js
+++ js/index.js
@@ -1,24 +1,30 @@
 function messageAnimate() {
-    $("#dot-one").delay(333).animate({opacity: 0}, 300);
-    $("#dot-two").delay(666).animate({opacity: 0}, 300);
-    $("#dot-three").animate({opacity: 0}, 300);
-    // messageAnimate();
-    setTimeout(3000);
-    $("#dot-one").delay(0).animate({opacity: 1}, 300);
-    $("#dot-two").delay(333).animate({opacity: 1}, 300);
-    $("#dot-three").delay(666).animate({opacity: 1}, 300);
-    setTimeout(messageAnimate, 999)
+    $("#dot-two").animate({opacity: 0}, 10);
+    $("#dot-three").animate({opacity: 0}, 10);
+    for (let i=0;i<100;i++) {
+        setTimeout($("#dot-two").animate({opacity: 1}, 300), 333);
+        setTimeout($("#dot-three").animate({opacity: 1}, 300), 666);
+        setTimeout($("#dot-one").animate({opacity: 0}, 300), 999);
+        setTimeout($("#dot-two").animate({opacity: 0}, 300), 1333);
+        setTimeout($("#dot-three").animate({opacity: 0}, 300), 1666);
+        setTimeout($("#dot-one").animate({opacity: 1}, 300), 1999);
+
+
+
+        // $("#dot-one").delay(999).animate({opacity: 1}, 300);
+        // $("#dot-two").delay(1333).animate({opacity: 1}, 300);
+        // $("#dot-three").delay(1666).animate({opacity: 1}, 300);
+        // $("#dot-one").delay(1999).animate({opacity: 0}, 300);
+        
+    }
 }
-var stopAnimation = false
 $('#message-icon').on('mouseenter', function() {
-    // $("#dot-one").animate({opacity: 0}, 3000);
-    // $("#dot-two").animate({opacity: 0}, 3000);
-    // $("#dot-three").animate({opacity: 0}, 3000);
-    messageAnimate();
-    // setTimeout(3000);
-    // $("#dot-one").delay(0).animate({opacity: 1}, 3000);
-    // $("#dot-two").delay(3330).animate({opacity: 1}, 3000);
-    // $("#dot-three").delay(6660).animate({opacity: 1}, 3000);
+    $("#dot-one").animate({opacity: 0}, 300);
+    $("#dot-two").delay(333).animate({opacity: 0}, 300);
+    $("#dot-three").delay(666).animate({opacity: 0}, 300);
+    $("#dot-one").delay(1000).animate({opacity: 1}, 300);
+    $("#dot-two").delay(1333).animate({opacity: 1}, 300);
+    $("#dot-three").delay(1666).animate({opacity: 1}, 300);
     
 
 }).on("mouseleave", function() {
@@ -30,4 +36,20 @@ $('#message-icon').on('mouseenter', function() {
     $("#dot-two").css({opacity: 1})
     $("#dot-three").css({opacity: 1})
     stopAnimation = true
+});
+
+$("a").on('click', function(event) {
+
+    if (this.hash !== "") {
+    event.preventDefault();
+
+    var hash = this.hash;
+
+    $('html, body').animate({
+        scrollTop: $(hash).offset().top
+    }, 800, function(){
+
+        window.location.hash = hash;
+    });
+    } 
 });
